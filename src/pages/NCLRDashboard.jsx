40fe5b234c4a231caa7ad0d0058fbfbd551d97cd
JosieEsi <josephine.kpodo@amalitech.org>
2024-11-28 22:04:07 +0000

    updated the dashboard for the pda africa website

--- src/pages/NCLRDashboard.jsx
+++ src/pages/NCLRDashboard.jsx
@@ -2,17 +2,13 @@ import React from "react";
 
 const NCLRDashboard = () => {
   return (
-    <section id="cclp-dashboard" className="max-container w-auto mt-24 h-auto">
+    <section id="cclp-dashboard" className="max-container w-auto mt-28 h-auto">
       <div>
-        {/* <iframe
-          src="https://pdaghana.com/child-abuse-tracker/"
+        <iframe
+          src="https://vsla-dashboards.herokuapp.com/nyonkopa-clr"
           style={{ width: "100%", height: "100vh", border: "none" }}
           title="Dashboard"
-        ></iframe> */}
-        <h1 className="h-screen font-bold font-poppins text-4xl text-center align-middle pt-28">
-          {" "}
-          Yet to be designed
-        </h1>
+        ></iframe>
       </div>
     </section>
   );
